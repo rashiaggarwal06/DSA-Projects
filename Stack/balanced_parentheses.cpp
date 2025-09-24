32b909492ddb06894093893794a55df3b12cc1b3
Rashi Aggarwal <146416686+rashiaggarwal06@users.noreply.github.com>
2025-09-24 21:52:07 +0530

    Update balanced_parentheses.cpp

--- Stack/balanced_parentheses.cpp
+++ Stack/balanced_parentheses.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;  
 // Function to check if the given string has balanced brackets
-bool isBalanced(const string &exp){
+bool isBalanced(const string &exp){ 
     stack<char> st; // Stack to store opening brackets
     // Traverse each character in the expression
     for(char ch : exp) {
